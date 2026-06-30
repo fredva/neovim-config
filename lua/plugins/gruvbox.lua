a6e1984c9114e84a9bf7b3658bc584fda1438fa5
Fredrik V. Mørken <fredrik@veryfast.biz>
2026-06-30 10:39:04 +0200

    gruvboxxxy

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "hard",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      background = "light",
+      colorscheme = "gruvbox",
+    },
+  },
+}
