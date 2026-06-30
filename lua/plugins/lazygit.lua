01be25e58abb7ddcc3e44685c651daddf0f55d39
Fredrik V. Mørken <fredrik@veryfast.biz>
2026-06-30 10:39:53 +0200

    color lazygit

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    lazygit = {
+      win = {
+        style = "lazygit",
+        wo = {
+          winhighlight = "Normal:Normal,NormalFloat:Normal",
+        },
+      },
+    },
+  },
+}
